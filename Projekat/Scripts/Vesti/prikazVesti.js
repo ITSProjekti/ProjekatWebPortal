965bdabae9f42151eb702f555ad3dc84ad6a5fa0
Student <Student@ADMINRG-CDNBDLD>
2018-10-17 13:58:35 +0200

    Sredjen login, i dropdown tri tacke, sredjene vesti, skinuta obavestenja

--- Projekat/Scripts/Vesti/prikazVesti.js
+++ Projekat/Scripts/Vesti/prikazVesti.js
@@ -27,8 +27,8 @@ function GetData() {
     "<div class=\"slikaNaslov\"><img class=\"card-img-top\" src=\"" + data[i].Thumbnail + "\" alt=\"Card image cap\"> <div class=\"naslovVesti\"><p class=\"card-text\">" + data[i].Naslov + "</p></div></div>" +
     "<div class=\"card-body\"><div class=\"preveiw\"> <p class=\"prvText\">" + data[i].KratakOpis + "</p></div> <hr/> <div class=\"prikazi\">" +
             "<form action=\"/Vesti/PrikaziVest\" method=\"get\">" +
-            "<input type=\"hidden\" name=\"Naslov\" value=\"" + data[i].Naslov + "\" />" +
-            "<input type=\"hidden\"  name=\"Datum\" value=\"" + jsDate.toLocaleDateString() + "\" />" +
+                            "<input type=\"hidden\" name=\"Naslov\" value=\"" + data[i].Naslov + "\" />" +
+                            "<input type=\"hidden\"  name=\"Datum\" value=\"" + jsDate.toLocaleDateString() + "\" />" + "<p class=\"card-text\" style=\"position: absolute; font-size: 13px; color: whitesmoke; top: 10px;\">" + jsDate.toLocaleDateString() + "</p>" +
             "<input type=\"submit\" class=\"btnsVestiPrikaz\" value=\"SAZNAJ VIŠE\" />" +
             "</form>" +
             "<form action=\"/Vesti/BrisanjeVesti\" method=\"post\">" +
