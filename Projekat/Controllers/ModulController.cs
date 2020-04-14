61a411252af07cc9c2d0e5c4981597698a7f6c74
Nikola Jovanovic <nikola19817@gmail.com>
2020-04-14 17:19:21 +0200

    Dodavanje kontrolera za Modul

---
+++ Projekat/Controllers/ModulController.cs
@@ -0,0 +1,43 @@
+﻿using Projekat.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Projekat.Controllers
+{
+    public class ModulController : Controller
+    {
+        private IMaterijalContext context;
+
+        // GET: Modul
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ModulPrikaz(int id)
+        {
+            context = new MaterijalContext();
+            int pID;
+            List<ModulModel> modeli;
+
+            try
+            {
+                pID = context.predmeti.FirstOrDefault(x => x.predmetId == id).predmetId;
+            }
+            catch { return View("FileNotFound"); }
+
+            if (pID != 0)
+            {
+                try
+                {
+                    modeli = context.moduli.Where(x => x.predmetId == pID).ToList();
+                }
+                catch { return new HttpStatusCodeResult(403); }
+
+                return View(modeli);
+            }
+            return View("FileNotFound");
+        }
+    }
+}
